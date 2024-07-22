184897212e7dd206053f9714c100d69c814991e1
Enrico Mosca <20000enrico@gmail.com>
2024-07-22 11:48:57 +0200

    Struttura progetto

---
+++ src/main/java/com/example/demo/ApiAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthorizationApplication.class, args);
+	}
+
+}
